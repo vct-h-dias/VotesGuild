d9d2bd1fff6343e3bcf55cde20ebfc91986e2abc
vct-h-dias <d.hugo@aluno.ifsp.edu.br>
2023-11-07 21:20:59 -0300

    signup UI

---
+++ script/signin.js
@@ -0,0 +1,81 @@
+const validForm = {
+    email: false,
+    password: false
+}
+
+const submitButton = document.getElementById('sign-in');
+const validateForm = () => {
+    console.log(validForm)
+    if (!!validForm.email && !!validForm.password) {
+        submitButton.removeAttribute("disabled");
+    } else {
+        submitButton.setAttribute("disabled", "disabled");
+    }
+}
+
+const login = async () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("pass").value;
+
+    const hex = randomRegex();
+
+    const hash = await hash256(password + hex);
+
+    const body = {
+        email: email,
+        password: hash,
+        salt: hex
+    }
+
+    console.log(body);
+    console.log(body.password.length);
+    console.log(body.salt.length);
+
+    console.log('Hello World');
+    console.log('Hello World');
+    window.location.href = './pages/votes.html'
+}
+
+const emailInput = document.getElementById('email');
+emailInput.addEventListener('input', (e) => {
+    const email = e.target.value;
+    console.log(email)
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    const validEmailError = document.getElementById('valid-email');
+    if (!emailRegex.test(email)) {
+        emailInput.style.borderColor = "red";
+        validEmailError.style.opacity = 1;
+        validEmailError.style.marginTop = "0";
+        validForm.email = false;
+    } else {
+        emailInput.style.borderColor = "#68F554";
+        validEmailError.style.opacity = 0;
+        validEmailError.style.marginTop = "-20px";
+        validForm.email = true;
+    }
+    validateForm();
+})
+
+passwordInput = document.getElementById('pass');
+passwordInput.addEventListener('input', (e) => {
+    const password = e.target.value;
+
+    const validPasswordError = document.getElementById('valid-pass');
+    if (password.length < 8) {
+        passwordInput.style.borderColor = "red";
+        validPasswordError.style.opacity = 1;
+        validPasswordError.style.marginTop = "0";
+        validForm.password = false;
+    } else {
+        passwordInput.style.borderColor = "#68F554";
+        validPasswordError.style.opacity = 0;
+        validPasswordError.style.marginTop = "-20px";
+        validForm.password = true;
+    }
+    validateForm();
+})
+
+const goToSignUp = () => {
+    window.location.href = './pages/signup.html'
+}
